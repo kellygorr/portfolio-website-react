b089fbf9832ab8a50b5af183a67ba5b5dabe4033
Kelly <kelly.a.gorr@gmail.com>
2020-09-11 17:21:17 -0700

    staggered grid update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,17 +2,20 @@ import * as React from 'react'
 import styled from 'styled-components/macro'
 import { Thumbnail } from './shared'
 import { projects } from './data'
+import { ThumbnailImage, transparentBase64, H3, Tags } from './shared/Thumbnail'
 
 export const Home: React.FC = () => {
 	const ref = React.useRef<HTMLDivElement>()
-
 	const [rowLength, setRowLength] = React.useState(0)
+	// Calculate missing cards
+
+	const unevenAmount = projects && projects.length % rowLength
+	const missingAmount = rowLength - unevenAmount
+
 	const handleResize = () => {
 		if (ref) {
 			const width = ref.current && ref.current.clientWidth
-
 			const childWidth = ref.current.children[0].clientWidth
-
 			const rowLength = Math.floor(width / childWidth)
 			setRowLength(rowLength)
 		}
@@ -31,24 +34,57 @@ export const Home: React.FC = () => {
 		<Gallery ref={ref}>
 			{projects.map((project, index) => {
 				const isRowEven = Math.floor(index / rowLength) % 2 === 0
-				const unevenAmount = projects.length % rowLength
-				const unevenDif = projects.length - unevenAmount
-				// If we have an uneven amount in the last row, we need to center these items
-				const isUnevenLastRow = index >= projects.length - unevenAmount
-				const unevenPercent = (50 - unevenDif) / 2
 
 				return (
 					<Thumbnail
 						key={project.details.header}
 						data={project.details}
 						style={{
-							right: isUnevenLastRow ? `-${unevenPercent}vw` : rowLength <= 2 ? 'initial' : isRowEven ? '30px' : '-30px',
+							right: isRowEven ? '30px' : '-30px',
 						}}
 					/>
 				)
 			})}
+			{missingAmount &&
+				Array.from(Array(missingAmount).keys()).map((x: number, index: number) => {
+					const isRowEven = Math.floor((index + projects.length) / rowLength) % 2 === 0
+					const tagLength = Array.from(Array(Math.floor(Math.random() * 3) + 1).keys())
+					return (
+						<BlankCard
+							key={x + index}
+							style={{
+								opacity: 0.5,
+								right: isRowEven ? '30px' : '-30px',
+							}}
+						>
+							<ThumbnailImage
+								style={{
+									backgroundImage: `url('data:image/png;base64,${transparentBase64}')`,
+								}}
+							/>
+							<H3>{RandomStr()}</H3>
+							<Tags>
+								[{' '}
+								{tagLength.map((x: number, index) => {
+									return <Tag key={x + index}>{tagLength.length !== index + 1 ? RandomStr() + ', ' : RandomStr()}</Tag>
+								})}{' '}
+								]
+							</Tags>
+						</BlankCard>
+					)
+				})}
 		</Gallery>
 	)
+}
+
+const RandomStr = () => {
+	const len = Math.floor(Math.random() * 10) + 3
+	const randomString = '01'
+	let ans = ''
+	for (var i = len; i > 0; i--) {
+		ans += randomString[Math.floor(Math.random() * randomString.length)]
+	}
+	return ans
 }
 
 export const Gallery = styled.div`
@@ -57,4 +93,16 @@ export const Gallery = styled.div`
 	display: grid;
 	grid-template-columns: [gallery] repeat(auto-fill, minmax(260px, 1fr));
 	grid-gap: 20px;
+`
+
+const BlankCard = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+
+const Tag = styled.span`
+	width: 100%;
+	padding-top: 3px;
+	text-align: center;
+	font-size: 0.9em;
 `
